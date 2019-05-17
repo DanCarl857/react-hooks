85df3f5815df212ea8dad93413cecf166e05dee0
Daniel Carlson <dancarl758@gmail.com>
2019-05-17 04:55:53 +0100

    Simple refactor

--- src/App.js
+++ src/App.js
@@ -4,15 +4,15 @@ const App = () => {
 
   const [count, setCount] = useState(0)
 
-  const increase = () => setCount(count + 1)
-  const decrease = () => setCount(count - 1)
-  const reset = () => setCount(0)
+  const handleIncrease = () => setCount(count + 1)
+  const handleDecrease = () => setCount(count - 1)
+  const handleReset = () => setCount(0)
 
   return (
     <div>
-      <button onClick={increase}>Increase</button>
-      <button onClick={decrease}>Decrease</button>
-      <button onClick={reset}>Reset</button>
+      <button onClick={handleIncrease}>Increase</button>
+      <button onClick={handleDecrease}>Decrease</button>
+      <button onClick={handleReset}>Reset</button>
       <h1>{count}</h1>
     </div>
   )
