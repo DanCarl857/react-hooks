3b6579039fec18fa9d4800df3182ef335daae44e
Daniel Carlson <dancarl758@gmail.com>
2019-05-17 05:12:16 +0100

    Simple refactor: using multiple useStates

--- src/App.js
+++ src/App.js
@@ -21,19 +21,11 @@ import React, { useState } from 'react';
 
 const App = () => {
 
-  const [state, setState] = useState({ city: '', country: ''})
+  const [city, setCity] = useState('')
+  const [country, setCountry] = useState('')
 
-  const handleCityChange = (event) => {
-    setState({
-      ...state, city: event.target.value
-    })
-  }
-
-  const handleCountryChange = (event) => {
-    setState({
-      ...state, country: event.target.value
-    })
-  }
+  const handleCityChange = (event) => setCity(event.target.value)
+  const handleCountryChange = (event) => setCountry(event.target.value)
 
   return (
     <form>
@@ -41,7 +33,7 @@ const App = () => {
         <input
           type="text"
           placeholder="City"
-          value={state.city}
+          value={city}
           onChange={handleCityChange}
         />
       </div>
@@ -49,13 +41,13 @@ const App = () => {
         <input
           type="text"
           placeholder="Country"
-          value={state.country}
+          value={country}
           onChange={handleCountryChange}
         />
       </div>
 
       <div>
-        You live in {`${state.city}, ${state.country}` }
+        You live in {`${city}, ${country}` }
       </div>
     </form>
   )
